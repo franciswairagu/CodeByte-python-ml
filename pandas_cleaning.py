119325968ffdbf8cb3b484100697bc883fbf52a0
wairagu <wairagu@(none)>
2026-08-12 13:01:20 +0300

    Pandas data cleaning challenge

---
+++ pandas_cleaning.py
@@ -0,0 +1,14 @@
+# =============================
+# Question
+# =============================
+
+"""
+You are provided with a csv sales_data_missing.csv containing detailed sales information
+Replace missing Promotion_id column with "No Promotion".
+Calculate the taotal sales for the specific product_id in a given month.
+Add a column each month's total sales to the dataset.
+There are only two months present in the data; order these columns in ascending order by month number.
+If there were no sales, fill the column with 0.0.
+Ensure that the dataset contains only order_id, product_id, promotion_id and the monthly_total_sale columns.
+Return the cleaned dataset as Numpy array.
+"""
